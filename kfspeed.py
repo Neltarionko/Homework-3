9cc239ea4f61eddc25fb98bdb49063fb19f6ec0b
Dorlerd <fedbar02@gmail.com>
2023-04-02 19:22:12 +0300

    Update kfspeed.py

--- kfspeed.py
+++ kfspeed.py
@@ -5,24 +5,32 @@ class KF1():
         self.dmove = dmove
         self.dtotal = 0
         self.mtotal = 0
+        self.a = 0
 
     def predict(self):
+        self.dtotal += self.a*0.1
         self.dtotal += self.dmove
         #print('d Total',self.dtotal)
         return self.mtotal,self.dtotal**0.5
 
     def sensGPS(self,v):
-        k = self.dtotal/(self.dtotal + self.dGPS)
+        self.a = self.dtotal
+        k = self.dtotal/(self.dtotal + self.dGPS+ 0.00000000000001)
         #print('k GPS',k)
         self.dtotal = k*self.dGPS
         self.mtotal = self.mtotal + k*(v - self.mtotal)
+        self.a -= self.dtotal
+        self.a *= -1
         return self.mtotal, self.dtotal**0.5
 
     def sensOBD(self,v):
-        k = self.dtotal/(self.dtotal + self.dOBD)
+        self.a = self.dtotal
+        k = self.dtotal/(self.dtotal + self.dOBD+ 0.00000000000001)
         #print('k OBD', k)
         self.dtotal = k*self.dOBD
         self.mtotal = self.mtotal + k*(v - self.mtotal)
+        self.a -= self.dtotal
+        self.a *= -1
         return self.mtotal, self.dtotal**0.5
 
     def updatedisp(self, dOBP, dGPS):

35cd2e2571d6a2d043f4205a9ae9905e411bac03
Dorlerd <fedbar02@gmail.com>
2023-04-02 17:35:23 +0300

    main

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from base import *
 from kfspeed import *
+import matplotlib.pyplot as plt
 
 GPSTime, DeviceTime, GPSSpeed, OBDSpeed, Longitude, Latitude, Bearing = values('data1.csv')
 
 kf = KF1(0.1,0.2)
-
-print(kf.predict(OBDSpeed[0]))
-print(kf.sens(GPSSpeed[0]))
-print(GPSTime[0])
-print(DeviceTime[0])
-for i in range(9):
-    print(DeviceTime[1+i])
-    print(kf.sens(1+i))
-print(kf.predict(OBDSpeed[11]))
-print(kf.sens(GPSSpeed[1]))
-print(GPSTime[1])
-print(DeviceTime[10])
+print(len(GPSTime),len(DeviceTime))
+for k in range(len(GPSTime)):
+    print('GPS time:',GPSTime[k],'OBD time:',DeviceTime[k*10])
+    for i in range(9):
+        DeviceTime[1+i]
+        #print(kf.sens(1+i))
+    #print(kf.predict(OBDSpeed[11]))
+    #print(kf.sens(GPSSpeed[1]))

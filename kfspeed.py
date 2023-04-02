d54daf081dc198c31faa50b03d79377165936d45
Dorlerd <fedbar02@gmail.com>
2023-04-02 17:24:56 +0300

    KFspeed

--- kfspeed.py
+++ kfspeed.py
@@ -1,16 +1,21 @@
 class KF1():
     def __init__(self,dmove,dsens):
-        self.dmove = dmove
-        self.dsens = dsens
+        self.dmove = dmove**2
+        self.dsens = dsens**2
         self.dtotal = 0
         self.mtotal = 0
-        self.k =
 
     def predict(self,v):
         self.mtotal += v
         self.dtotal += self.dmove
         return self.mtotal,self.dtotal
     def sens(self,v):
-        k
+        k = self.dtotal/(self.dtotal+self.dsens)
+        self.dtotal = k*self.dsens
+        self.mtotal = self.mtotal + k*(v - self.mtotal)
+        return self.mtotal, self.dtotal
 
+#kf = KF1(0.1,0.2)
+#print(kf.predict(1))
+#print(kf.sens(2))
 

4766da825bf149b825a3eb4c909663f759356836
Dorlerd <fedbar02@gmail.com>
2023-04-03 15:40:18 +0300

    Revert "Update kfspeed.py"
    This reverts commit 9cc239ea4f61eddc25fb98bdb49063fb19f6ec0b.

--- kfspeed.py
+++ kfspeed.py
@@ -5,32 +5,24 @@ class KF1():
         self.dmove = dmove
         self.dtotal = 0
         self.mtotal = 0
-        self.a = 0
 
     def predict(self):
-        self.dtotal += self.a*0.1
         self.dtotal += self.dmove
         #print('d Total',self.dtotal)
         return self.mtotal,self.dtotal**0.5
 
     def sensGPS(self,v):
-        self.a = self.dtotal
-        k = self.dtotal/(self.dtotal + self.dGPS+ 0.00000000000001)
+        k = self.dtotal/(self.dtotal + self.dGPS)
         #print('k GPS',k)
         self.dtotal = k*self.dGPS
         self.mtotal = self.mtotal + k*(v - self.mtotal)
-        self.a -= self.dtotal
-        self.a *= -1
         return self.mtotal, self.dtotal**0.5
 
     def sensOBD(self,v):
-        self.a = self.dtotal
-        k = self.dtotal/(self.dtotal + self.dOBD+ 0.00000000000001)
+        k = self.dtotal/(self.dtotal + self.dOBD)
         #print('k OBD', k)
         self.dtotal = k*self.dOBD
         self.mtotal = self.mtotal + k*(v - self.mtotal)
-        self.a -= self.dtotal
-        self.a *= -1
         return self.mtotal, self.dtotal**0.5
 
     def updatedisp(self, dOBP, dGPS):

1186a6d987fef9a5c121cfee5df3d2003ff2ce1f
Dorlerd <fedbar02@gmail.com>
2023-04-03 16:00:36 +0300

    Update kfspeed.py

--- kfspeed.py
+++ kfspeed.py
@@ -1,13 +1,14 @@
 class KF1():
-    def __init__(self,dmove,dOBP, dGPS):
+    def __init__(self,dOBP, dGPS):
         self.dOBD = dOBP ** 2
         self.dGPS = dGPS ** 2
-        self.dmove = dmove
         self.dtotal = 0
         self.mtotal = 0
+        self.dmove = (0.1 + self.mtotal/10)**2
         self.a = 0
 
     def predict(self):
+        self.dmove = (0.1 + self.mtotal/10)**2
         self.dtotal += self.a*0.1
         self.dtotal += self.dmove
         #print('d Total',self.dtotal)

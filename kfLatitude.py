530ad3ccb92a62dbc2f99035d3a3a9466e97dba7
Dorlerd <fedbar02@gmail.com>
2023-04-03 21:00:50 +0300

    Update kfLatitude.py

--- kfLatitude.py
+++ kfLatitude.py
@@ -6,3 +6,9 @@ class KF1():
         self.mtotal = 0
         self.dmove = 0
         self.a = 0
+    def predict(self):
+        self.dmove = (0.1 + self.mtotal/10)**2
+        self.mtotal += self.a*0.1
+        self.dtotal += self.dmove
+        #print('d Total',self.dtotal)
+        return self.mtotal,self.dtotal**0.5

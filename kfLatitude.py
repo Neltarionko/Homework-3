d56bed8bf35db4d582a3165a3e6220576e442a9e
Dorlerd <fedbar02@gmail.com>
2023-04-03 20:59:34 +0300

    Update kfLatitude.py

--- kfLatitude.py
+++ kfLatitude.py
@@ -1,8 +1,8 @@
-class KF2():
-    def __init__(self,dOBP, dGPS):
-        self.dOBD = dOBP ** 2
-        self.dGPS = dGPS ** 2
+class KF1():
+    def __init__(self):
+        self.dOBD = 0
+        self.dGPS = 0
         self.dtotal = 0
         self.mtotal = 0
-        self.dmove = (0.1 + self.mtotal/10)**2
+        self.dmove = 0
         self.a = 0

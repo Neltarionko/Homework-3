15d4fff44f79f0cdb03ae069449e31551689e471
Dorlerd <fedbar02@gmail.com>
2023-04-04 09:01:28 +0300

    Update mainpos.py

--- mainpos.py
+++ mainpos.py
@@ -9,7 +9,7 @@ print(len(Bearing))
 for k in range(len(GPSTime)):
     try:
         kf.predict()
-        x.append(kf.sens(Latitude[k*10],GPSSpeed[k],Bearing[k*10]))
+        x.append(kf.sens(Latitude[k],GPSSpeed[k],Bearing[k]))
     except:
         continue
 
@@ -17,19 +17,19 @@ kf2 = KFy(Longitude[0])
 y = []
 for k in range(len(GPSTime)):
     kf2.predict()
-    y.append(kf2.sens(Longitude[k*10],GPSSpeed[k],Bearing[k*10]))
+    y.append(kf2.sens(Longitude[k],GPSSpeed[k],Bearing[k]))
 
-
+GPSTime = np.array(GPSTime)
 x = np.array(x)
 pointy2 = np.array([[x[i,0] + x[i,1],x[i,0] - x[i,1]] for i in range(len(GPSTime))])
-plt.plot(GPSTime, x[:,0],label='Матожидание текущего положения',color = 'g')
-#plt.plot(GPSTime,pointy2[:,0],label='дисперсия движения',color = 'm')
-#plt.plot(GPSTime,pointy2[:,1],color = 'm')
+plt.plot(GPSTime[:]/60 + 18, x[:,0],label='Матожидание текущего положения',color = 'g')
+plt.plot(GPSTime[:]/60 + 18,pointy2[:,0],label='дисперсия движения',color = 'm')
+plt.plot(GPSTime[:]/60 + 18,pointy2[:,1],color = 'm')
 plt.show()
 
 y = np.array(y)
 pointy2 = np.array([[y[i,0] + y[i,1],y[i,0] - y[i,1]] for i in range(len(GPSTime))])
-plt.plot(GPSTime, y[:,0],label='Матожидание текущего положения',color = 'g')
-#plt.plot(GPSTime,pointy2[:,0],label='дисперсия движения',color = 'm')
-#plt.plot(GPSTime,pointy2[:,1],color = 'm')
+plt.plot(GPSTime[:]/60 + 18, y[:,0],label='Матожидание текущего положения',color = 'g')
+plt.plot(GPSTime[:]/60 + 18,pointy2[:,0],label='дисперсия движения',color = 'm')
+plt.plot(GPSTime[:]/60 + 18,pointy2[:,1],color = 'm')
 plt.show()

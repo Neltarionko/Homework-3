5a2bfb17b03f3c554cbfc93e56e412985af6d80d
Dorlerd <fedbar02@gmail.com>
2023-04-05 14:33:19 +0300

    Update mainpos.py

--- mainpos.py
+++ mainpos.py
@@ -28,10 +28,25 @@ plt.plot(GPSTime[:]/60 + 18,pointy2[:,1],color = 'm')
 plt.legend()
 plt.show()
 
+
 y = np.array(y)
 pointy2 = np.array([[y[i,0] + y[i,1],y[i,0] - y[i,1]] for i in range(len(GPSTime))])
 plt.plot(GPSTime[:]/60 + 18, y[:,0],label='Матожидание текущего положения',color = 'g')
 plt.plot(GPSTime[:]/60 + 18,pointy2[:,0],label='дисперсия движения',color = 'm')
 plt.plot(GPSTime[:]/60 + 18,pointy2[:,1],color = 'm')
+plt.legend()
+plt.show()
+
+
+timewidth = 30*1
+time = 25
+rangeleft = int(((time-18)*60)-timewidth)
+rangeright = int(((time-18)*60)+timewidth)
+plotx = [GPSTime[i]/60 + 18 for i in range(rangeleft,rangeright)]
+ploty = [x[i,0] for i in range(rangeleft,rangeright)]
+ploty2 = np.array([[x[i,0] + x[i,1],x[i,0] - x[i,1]] for i in range(rangeleft,rangeright)])
+plt.plot(plotx,ploty,label='движение на 25 минуте в течении минутыч',color = 'g')
+plt.plot(plotx,ploty2[:,0],label='дисперсия движения',color = 'm')
+plt.plot(plotx,ploty2[:,1],color = 'm')
 plt.legend()
 plt.show()

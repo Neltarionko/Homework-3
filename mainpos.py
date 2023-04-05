706d6b879d7468d164ef1e75e5f3a4442f6723d2
Dorlerd <fedbar02@gmail.com>
2023-04-05 14:27:24 +0300

    Update mainpos.py

--- mainpos.py
+++ mainpos.py
@@ -25,6 +25,7 @@ pointy2 = np.array([[x[i,0] + x[i,1],x[i,0] - x[i,1]] for i in range(len(GPSTime
 plt.plot(GPSTime[:]/60 + 18, x[:,0],label='Матожидание текущего положения',color = 'g')
 plt.plot(GPSTime[:]/60 + 18,pointy2[:,0],label='дисперсия движения',color = 'm')
 plt.plot(GPSTime[:]/60 + 18,pointy2[:,1],color = 'm')
+plt.legend()
 plt.show()
 
 y = np.array(y)
@@ -32,4 +33,5 @@ pointy2 = np.array([[y[i,0] + y[i,1],y[i,0] - y[i,1]] for i in range(len(GPSTime
 plt.plot(GPSTime[:]/60 + 18, y[:,0],label='Матожидание текущего положения',color = 'g')
 plt.plot(GPSTime[:]/60 + 18,pointy2[:,0],label='дисперсия движения',color = 'm')
 plt.plot(GPSTime[:]/60 + 18,pointy2[:,1],color = 'm')
+plt.legend()
 plt.show()
